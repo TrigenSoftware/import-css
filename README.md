70d5d0c946a7203f0d6414810fdfde51ce909446
Dan Green <danon0404@gmail.com>
2017-02-14 19:05:04 +0700

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+[![NPM](https://nodei.co/npm/import-css.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/import-css/)
+
+# import-css
+
+Load CSS asynchronously without render blocking.
+
+# Getting Started
+
+ Install with npm
+```bash
+npm i import-css
+```
+or
+```bash
+yarn add import-css
+```
+
+# About
+
+`import-css` contains 3 variants of `importCSS` function
+
+1) `link.js` variant uses `<link>` tag. If you want to import multiple styles for "progressive loading" recommended move all function calls before `</body>`.
+2) `link-and-body.js` also uses`<link>` tag, but this variant a little lighter than `link.js` because of restricted to use only in `<body>`.
+3) `xhr.js` obviously uses XMLHttpRequest to load styles. For earlier loading start recommended to call this function only in `<head>`. `xhr.js` variant loads styles fully async and more faster (if started at `<head>`). But with `<link>` variant you have possibilities to include styles from other hosts without CORS. In IE and Edge `xhr.js` loads styles in parallel unlike `<link>` variants. 
---
+++ src/link.js
@@ -0,0 +1,99 @@
+/**
+ * Load CSS asynchronously without render blocking.
+ * This variant uses <link> tag.
+ *
+ * If you want to import multiple styles for "progressive loading"
+ * recommended to move all function calls before </body>.
+ *
+ * XHR variant loads styles fully async and more faster,
+ * becouse loading starts at <head>. But with <link> variant
+ * you have possibilities to include styles from other hosts
+ * without CORS.
+ */
+
+const $    = document,
+	root   = $.body || $.getElementsByTagName('head')[0],
+	sheets = $.styleSheets;
+
+let queue = [];
+
+// wait until body is defined before injecting link.
+// This ensures a non-blocking load in IE11.
+function ready(cb) {
+
+	if ($.body) {
+		cb();
+		return;
+	}
+
+	setTimeout(() =>
+		ready(cb)
+	);
+}
+
+// A method (exposed on return object for external use) that mimics onload
+// by polling $ument.styleSheets until it includes the new sheet.
+function onLoadLinkDefined(resolvedHref, cb) {
+
+	let i = sheets.length;
+
+	while (i--) {
+		if (sheets[i].href === resolvedHref) {
+			cb();
+			return;
+		}
+	}
+
+	setTimeout(() =>
+		onLoadLinkDefined(resolvedHref, cb)
+	);
+}
+
+export default function importCSS(href, media) {
+
+	const link = $.createElement('link');
+
+	let loaded = false;
+
+	link.rel = 'stylesheet';
+	link.href = href;
+	link.media = 'only x'; // temporarily set media to something inapplicable to ensure it'll fetch without blocking render
+
+	queue.push(() => {
+
+		if (loaded) {
+			link.media = media || 'all';
+		}
+
+		return loaded;
+	});
+
+	// `insertBefore` is used instead of `appendChild`,
+	// for safety re: http://www.paulirish.com/2011/surefire-dom-element-insertion/
+	ready(() =>
+		root.insertBefore(link, null)
+	);
+
+	function onLoad() {
+
+		if (link.addEventListener) {
+			link.removeEventListener('load', onLoad);
+		}
+
+		loaded = true;
+
+		let prev = true;
+
+		queue = queue.filter(_ =>
+			!(prev = prev && _())
+		);
+	}
+
+	// once loaded, set link's media back to `all`
+	// so that the stylesheet applies once it loads
+	if (link.addEventListener) {
+		link.addEventListener('load', onLoad);
+	}
+
+	onLoadLinkDefined(link.href, onLoad);
+}
